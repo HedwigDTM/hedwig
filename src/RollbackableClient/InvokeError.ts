11b7b9f88606164ce9ba5b420dcafcee3e5bac79
Yoav Richter <richter.yoav30@gmail.com>
2024-08-07 12:53:02 +0300

    Create new error for clients invocation

---
+++ src/RollbackableClient/InvokeError.ts
@@ -0,0 +1,9 @@
+/**
+ * Custom error class for errors during client invocation.
+ */
+export default class InvocationError extends Error {
+    constructor(message = '') {
+        super(message);
+        this.name = "RestoreError";
+    }
+}
