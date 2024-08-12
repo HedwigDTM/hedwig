2d740a8711fd2fca135b4dd270e819fd7b54afb7
Yoav Richter <richter.yoav30@gmail.com>
2024-08-12 12:53:40 +0300

    add testing fo deletion

--- test/S3Client/S3Client.test.ts
+++ test/S3Client/S3Client.test.ts
@@ -4,11 +4,13 @@ import {
   HeadObjectCommand,
   GetObjectCommand,
   CopyObjectCommand,
+  DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
 import { describe, expect, it, vi } from "vitest";
 import { S3Client, S3Params } from "../../src/S3Client/S3Client";
 import { S3RollbackStrategy } from "../../src/S3Client/S3RollbackStrategy";
 import { Readable } from "stream";
+import { S3BackupError } from "../../src/S3Client/S3RollbackFactory";
 
 // Create mock stream
 const mockStream = new Readable();
@@ -18,22 +20,39 @@ mockStream.push(null);
 
 // Create mock aws connection
 const mockS3 = new AWSS3Client({});
-const mockSend = vi.fn((command) => {
+const mockSendFileDoesntExists = vi.fn((command) => {
   if (command instanceof PutObjectCommand) {
     return Promise.resolve();
   } else if (command instanceof HeadObjectCommand) {
+    return Promise.reject({});
+  } else if (command instanceof GetObjectCommand) {
+    return Promise.reject({});
+  } else if (command instanceof CopyObjectCommand) {
     return Promise.resolve({});
+  } else if (command instanceof DeleteObjectCommand) {
+    return Promise.reject({});
+  } else {
+    return Promise.reject(new Error("Unsupported command"));
+  }
+});
+const mockSendFileExists = vi.fn((command) => {
+  if (command instanceof PutObjectCommand) {
+    return Promise.resolve();
+  } else if (command instanceof HeadObjectCommand) {
+    return Promise.resolve({ Body: mockStream });
   } else if (command instanceof GetObjectCommand) {
     return Promise.resolve({ Body: mockStream });
   } else if (command instanceof CopyObjectCommand) {
+    return Promise.resolve({});
+  } else if (command instanceof DeleteObjectCommand) {
     return Promise.resolve({});
   } else {
     return Promise.reject(new Error("Unsupported command"));
   }
 });
-mockS3.send = mockSend;
 
-describe("S3Client - put an object to S3 successfully", () => {
+describe("S3Client - put new object to S3", () => {
+  mockS3.send = mockSendFileDoesntExists;
   const params: S3Params = {
     Bucket: "test-bucket",
     Key: "test",
@@ -55,5 +74,67 @@ describe("S3Client - put an object to S3 successfully", () => {
       S3RollbackStrategy.DUPLICATE_FILE
     );
     expect(async () => await client.putObject(params)).not.toThrow();
+  });
+});
+
+describe("S3Client - put existing object to S3", () => {
+  mockS3.send = mockSendFileExists;
+  const params: S3Params = {
+    Bucket: "test-bucket",
+    Key: "test",
+    Body: Buffer.from("test", "utf-8"),
+  };
+
+  it("IN_MEMORY strategy", async () => {
+    const client: S3Client = new S3Client(
+      "1",
+      mockS3,
+      S3RollbackStrategy.IN_MEMORY
+    );
+    expect(async () => await client.putObject(params)).not.toThrow();
+  });
+  it("DUPLICATE strategy", async () => {
+    const client: S3Client = new S3Client(
+      "1",
+      mockS3,
+      S3RollbackStrategy.DUPLICATE_FILE
+    );
+    expect(async () => await client.putObject(params)).not.toThrow();
+  });
+});
+
+describe("S3Client - delete object from S3", () => {
+  const params: S3Params = {
+    Bucket: "test-bucket",
+    Key: "test",
+    Body: Buffer.from("test", "utf-8"),
+  };
+
+  it("File exsists, IN_MEMORY strategy", async () => {
+    mockS3.send = mockSendFileExists;
+    const client: S3Client = new S3Client(
+      "1",
+      mockS3,
+      S3RollbackStrategy.IN_MEMORY
+    );
+    expect(async () => await client.deleteObject(params)).not.toThrow();
+  });
+  it("File exsists, DUPLICATE strategy", async () => {
+    mockS3.send = mockSendFileExists;
+    const client: S3Client = new S3Client(
+      "1",
+      mockS3,
+      S3RollbackStrategy.DUPLICATE_FILE
+    );
+    expect(async () => await client.deleteObject(params)).not.toThrow();
+  });
+  it("File doesn't exists, IN_MEMORY strategy", async () => {
+    mockS3.send = mockSendFileDoesntExists;
+    const client: S3Client = new S3Client(
+        "1",
+        mockS3,
+        S3RollbackStrategy.IN_MEMORY
+      );
+    expect(async () => await client.deleteObject(params)).rejects.toThrow(S3BackupError);
   });
 });
